a61690469fe1a272dd818c8c624f8f6b6edb8db8
BeJIayLLI <79015676+bejiayll@users.noreply.github.com>
2024-12-26 11:55:15 +0400

    Pre-release 2

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import connection
 
 import customtkinter as tk
+from tkinter.messagebox import showerror
+import socket
 import threading
 import pickle
-import socket
 import os
 import rsa
+
+
 class App:
     def __init__(self):
 
-        self.server_ip = "localhost"
-        self.server_port = 12345
+        self.server_ip = ""
+        self.server_port = 0
 
         self.friend_key_path = "./config/freinds/public.txt"
         self.public_key_path = "./config/public.txt"
@@ -25,7 +28,29 @@ class App:
         self.app.resizable(width=False, height=False)
         self.app.title("Forq's anonimous chat")
 
+        try: 
+            with open(self.private_key_path, "rb") as file:
+                unpickler = pickle.Unpickler(file)
+                self.my_private_key = unpickler.load()
+
+            with open(self.friend_key_path, "rb") as file:
+                unpickler = pickle.Unpickler(file)
+                self.freind_public_key = unpickler.load()
+        except:
+            pass
+
         self.update_interface()
+
+        self.app.protocol("WM_DELETE_WINDOW", lambda: self.exit())
+
+    def exit(self):
+            try:
+                self.send_protocol("EXIT") 
+                self.client.close()
+            except:
+                pass
+            self.app.destroy()
+            quit()
 
     def update_interface(self):
 
@@ -35,7 +60,7 @@ class App:
         
         self.tabview.pack(anchor="center")
 
-        ## Message Tab
+        ## Message Tab ##
         self.chat_messages = tk.CTkScrollableFrame(self.tab_msg, bg_color="transparent", width=720, height=460)
         self.chat_messages.pack(padx= 10, pady= 10, anchor= "nw")
 
@@ -45,12 +70,36 @@ class App:
         self.message_button = tk.CTkButton(self.tab_msg, text="Send", width=40, height=30, command=self.send_msg_button)
         self.message_button.pack(side="right", fill="both")
 
-        ## Settings tab
+        ## Settings tab ##
+        self.ip_label = tk.CTkLabel(self.tab_settings, text="ip")
+        self.port_label = tk.CTkLabel(self.tab_settings, text="port")
 
-        self.settings_frame = tk.CTkFrame(self.tab_settings, bg_color="transparent", width=780, height=550) 
-        self.settings_frame.pack(anchor = "center")
+        self.ip_label.grid(row=0, column=0, padx=10)
+        self.port_label.grid(row=0, column=3, padx=10)
+
+        self.ip_input = tk.CTkEntry(self.tab_settings, width=300, height= 20)
+        self.port_input = tk.CTkEntry(self.tab_settings, width=60, height= 20)
+
+        self.ip_input.grid(row=0, column=1, padx=10)
+        self.port_input.grid(row=0, column=2, padx=10)
+
+        self.settings_connect_button = tk.CTkButton(self.tab_settings, text="Connect", fg_color="#017003", corner_radius=20, command=self.update_connection)
+        self.settings_connect_button.grid(row=1, column=1, pady=10)
+        self.settings_connect_button = tk.CTkButton(self.tab_settings, text="Generate\nencryption keys", fg_color="#017003", corner_radius=20, command=self.generate_encryption_keys)
+        self.settings_connect_button.grid(row=2, column=1, pady=20)
 
         self.checker_keys()
+
+    def update_connection(self):
+        server_ip = self.ip_input.get() if self.ip_input.get() != None else ""
+        server_port = self.port_input.get() if self.port_input.get() != None else ""
+        if server_ip != "" and server_port != "":
+            self.server_ip = server_ip
+            self.server_port = (int)(server_port)
+            self.connect_to_socket()
+        else: 
+            showerror(title="Connection error", message="The ip or port is incorrect")
+        
 
     def show_error_msg(self, text):
         msg_frame = tk.CTkFrame(self.chat_messages, fg_color="transparent")
@@ -69,7 +118,16 @@ class App:
         msg = tk.CTkLabel(msg_frame, text=text, fg_color="#13629e", corner_radius=10)
         msg_frame.pack(anchor='nw')        
         msg.pack(padx=10, pady=10, anchor="nw")
+        
+    def generate_encryption_keys(self):
+        public, private = rsa.newkeys(512)
 
+        with open("config\\public.txt", "wb") as file:
+            pickle.dump(public, file)
+        with open("config\\private.txt", "wb") as file:
+            pickle.dump(private, file)
+
+        self.my_private_key = private
 
     def checker_keys(self):
         if (os.path.exists(self.private_key_path) and os.path.exists(self.public_key_path)):
@@ -89,8 +147,8 @@ class App:
             self.client.connect((self.server_ip, self.server_port))
             threading.Thread(target=self.message_handler).start()
             return
-        except:
-            self.show_error_msg("Failed to connect server")
+        except socket.error:
+            self.show_error_msg("Failed connect to server")
             return
 
     def message_handler(self):
@@ -99,8 +157,8 @@ class App:
             message_decode = (str)(message.decode("utf-8"))
             if len(message_decode) > 0:
                 try:
-                    #message_decrypt = rsa.decrypt(message_decode, self.my_private_key)
-                    self.show_getted_msg(message_decode)
+                    message_decrypt = rsa.decrypt(message_decode, self.my_private_key)
+                    self.show_getted_msg(message_decrypt)
                 except rsa.DecryptionError:
                     continue
     
@@ -112,10 +170,13 @@ class App:
 
     def send_message(self, text):
         #try:
-            #message = rsa.encrypt(self.freind_public_key)
+            message = rsa.encrypt(text, self.freind_public_key)
+            self.client.send(bytes(message, encoding="utf-8"))
+        # except:
+            # self.show_error_msg("An error occurred while sending a message")
+
+    def send_protocol(self, text):
         self.client.send(bytes(text, encoding="utf-8"))
-        #except:
-            #self.show_error_msg("An error occurred while sending a message")
 
     def run(self):
         self.app.mainloop()
